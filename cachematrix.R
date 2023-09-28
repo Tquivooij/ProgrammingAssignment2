30018c7ca4f239ccc71839bfd607b3719da200d3
Tquivooij <tim.quivooy@gmail.com>
2023-09-28 13:33:09 +0200

    Submision of programming assignment 2 lexical scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming assignment 2 submission by Tim Quivooij
+## 28-9-2023
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
+## based on example function makeVector
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    ## method to set the matrix
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    ## method to get the matrix
+    get <- function() {
+        ## return the matrix
+        x
+    }
+    ## Method to set the inverse of the matrix
+    setinverse <- function(inverse) {
+        ## set the inverse
+        m <<- inverse
+    }
+    ## Method to get the inverse of the matrix
+    getinverse <- function() {
+        ## return the inverse 
+        m
+    }
+    
+    ## return a list of the methods
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned
+## by makeCacheMatrix.
+## based on example function cachemean
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+    }
+    
+    ## get the matrix
+    data <- x$get()
+    
+    ## calculate the inverse 
+    m <- solve(data) %*% data
+    
+    ## set inverse of the matrix
+    x$setinverse(m)
+    
+    ## return the (inverse) matrix
+    m
 }
